84e16d1a53125c3f1aac3926442d529bd517a283
0megq <nvatanshenas55@gmail.com>
2022-05-29 22:58:55 -0700

    Added Red Frog Scene

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -8,4 +8,4 @@ var current_level: int = 1
 func change_level() -> void:
 	current_level += 1
 	print(current_level)
-	get_tree().change_scene(level_array[current_level-1])
+#	get_tree().change_scene(level_array[current_level-1])
